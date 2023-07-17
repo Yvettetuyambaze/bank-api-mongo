4e8c1314ef42f5993296e0ae079945b51227376c
Yvettetuyambaze <tuyambazeyvette@gmail.com>
2023-07-17 18:06:24 +0200

    Update transactionRoutes.js

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,4 +1,3 @@
-// routes/transactionRoutes.js
 const express = require('express');
 const router = express.Router();
 const Transaction = require('../models/Transaction');
@@ -37,6 +36,94 @@ router.post('/transactions', async (req, res) => {
     await bankAccount.save();
 
     res.status(201).json({ success: true, data: transaction });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+// Get all transactions
+router.get('/transactions', async (req, res) => {
+  try {
+    const transactions = await Transaction.find();
+    res.json({ success: true, data: transactions });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+// Get transactions by account number
+router.get('/transactions/:accountNumber', async (req, res) => {
+  try {
+    const accountNumber = req.params.accountNumber;
+    const transactions = await Transaction.find({ accountNumber });
+    res.json({ success: true, data: transactions });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+
+// Get a transaction by ID
+router.get('/transactions/:transactionId', async (req, res) => {
+  try {
+    const transactionId = req.params.transactionId;
+    const transaction = await Transaction.findById(transactionId);
+    
+    if (!transaction) {
+      return res.status(404).json({ success: false, message: 'Transaction not found' });
+    }
+    
+    res.json({ success: true, data: transaction });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+// Update a transaction by ID
+router.put('/transactions/:transactionId', async (req, res) => {
+  try {
+    const transactionId = req.params.transactionId;
+    const { accountNumber, amount, transactionType } = req.body;
+    
+    let transaction = await Transaction.findById(transactionId);
+    
+    if (!transaction) {
+      return res.status(404).json({ success: false, message: 'Transaction not found' });
+    }
+    
+    const bankAccount = await BankAccount.findOne({ accountNumber });
+    
+    if (!bankAccount) {
+      return res.status(404).json({ success: false, message: 'Bank account not found' });
+    }
+    
+    // Update transaction details
+    transaction.accountNumber = accountNumber;
+    transaction.amount = amount;
+    transaction.balance = bankAccount.balance;
+    transaction.transactionType = transactionType;
+    
+    await transaction.save();
+    
+    res.json({ success: true, data: transaction });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+// Delete a transaction by ID
+router.delete('/transactions/:transactionId', async (req, res) => {
+  try {
+    const transactionId = req.params.transactionId;
+    const transaction = await Transaction.findById(transactionId);
+    
+    if (!transaction) {
+      return res.status(404).json({ success: false, message: 'Transaction not found' });
+    }
+    
+    await transaction.remove();
+    
+    res.json({ success: true, data: transaction });
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
   }
