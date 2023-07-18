c3d25ffdd7263964a11d60b0c639b18731c1c75e
Yvettetuyambaze <tuyambazeyvette@gmail.com>
2023-07-18 18:50:02 +0200

    Create feedback.js

---
+++ src/controllers/feedback.js
@@ -0,0 +1,12 @@
+//use class
+//use validation
+//remove server.js
+//configure using environment variable
+//remove test
+//use const in routes
+//use find logics in service to be reusable
+//use middleware
+//success remove
+//use ! instead of
+//deploying
+//
